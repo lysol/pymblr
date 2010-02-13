be41f737986636ac6465057bd7da8e2ddeaac899
Derek Arnold <derek@io.local>
2010-02-13 12:51:19 -0600

    Correcting unchecked exception when URL is not text/html

--- pymblr.py
+++ pymblr.py
@@ -209,8 +209,12 @@ class Api:
             # Video found
             br = Browser()
             br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.5.1-1.fc9 Firefox/3.5.1')]
-            br.open(url)
-            post = self.write_video(data = url, caption = br.title() + "\n<br />" +caption)
+            try:
+                br.open(url)
+                title = br.title()
+            except:
+                title = ''
+            post = self.write_video(data = url, caption = title + "\n<br />" +caption)
             return post
         
         elif re.match('[jpg|jpeg|gif|png|bmp]$',url, re.I):
