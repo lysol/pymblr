175f4f1c0749dacf2c1062b42614dc0ff10dbf88
Derek Arnold <github@vyr.us>
2011-02-06 18:34:28 -0600

    Updated setup.py to conform with pypi.

--- setup.py
+++ setup.py
@@ -2,10 +2,27 @@ from distutils.core import setup
 
 setup(name='pymblr',
       version='0.2.22',
-      description='Python wrapper for Tumblr API',
+      description='Python wrapper for Tumblr API. Original version by ' + \
+        'Ryan A. Cox.',
       author='Derek Arnold',
       author_email='derek@dderek.com',
       license='Apache License 2.0',
       url='http://github.com/lysol/pymblr',
       py_modules=['pymblr'],
+      classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ),
+      requires=(
+          'ElementTree',
+          'httplib2',
+          'poster',
+          'mechanize',
+          'simplejson'
+          ),
+      download_url="http://github.com/lysol/pymblr/tarball"
      )
